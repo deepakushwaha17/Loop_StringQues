62ac5aceea3d3af6616b520d3e7a5a3acf1e18d6
Deepa <deepakushwaha155@gmail.com>
2022-11-20 21:58:44 +0530

    fourth commit

---
+++ Pattern4.java
@@ -0,0 +1,22 @@
+/* Program to print the pattern:
+            @ @ @ @ @ @
+            @ @ @ @ @
+            @ @ @ @
+            @ @ @
+            @ @
+            @             */
+class Pattern4 
+{
+    public static void main(String[] args) 
+    {
+        int i,j;
+        for(i=6;i>=1;i--)  
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("@ ");
+            }
+            System.out.println();
+        } 
+    }
+}
