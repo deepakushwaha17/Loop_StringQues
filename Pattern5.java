22ac1743216f35d99b7f86ea56ce57e18b3522b4
Deepa <deepakushwaha155@gmail.com>
2022-11-20 22:07:05 +0530

    fourth commit

---
+++ Pattern5.java
@@ -0,0 +1,24 @@
+/* Program to print the pattern:
+            #  
+            # #
+            # # #
+            # # # #
+            # # # # #
+            # # # # # #    
+*/
+class Pattern5
+{
+    public static void main(String[] args) 
+    {
+        int i,j;
+        for(i=1;i<=6;i++)  
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("# ");
+            }
+            System.out.println();
+        } 
+    }
+}
+            
