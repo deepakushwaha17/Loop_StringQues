c2a5d5cf3e35c724d8ba8b3a2771a694c270259e
Deepa <deepakushwaha155@gmail.com>
2022-11-23 20:40:19 +0530

    seventh commit

---
+++ Pattern9.java
@@ -0,0 +1,25 @@
+/*Program to print the pattern:
+           A A A A A
+           B B B B B
+           C C C C C
+           D D D D D
+           E E E E E
+           F F F F F
+ */
+public class Pattern9
+{
+    public static void main(String[] args) 
+    {
+        //int A=65;
+        for(int i=65;i<=70;i++)   
+        {
+            for(int j=1;j<=5;j++)
+            {
+                System.out.print((char)i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
