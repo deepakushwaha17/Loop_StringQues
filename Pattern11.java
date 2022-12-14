64cf320b0a5795ff009f43e2b430ae374bfd56e2
Deepa <deepakushwaha155@gmail.com>
2022-12-14 21:35:59 +0530

    11th commit

---
+++ Pattern11.java
@@ -0,0 +1,21 @@
+/*Program to print the pattern:
+            1 
+            2 3 
+            4 5 6 
+            7 8 9 10 
+            11 12 13 14 15    */
+public class Pattern11
+{            
+    public static void main(String[] args) 
+    {
+        int i,j,k=1;
+        for(i=1;i<=5;i++) 
+        {
+            for(j=1;j<i+1;j++) 
+            {
+                System.out.print(k++ +" ");
+            }
+            System.out.println();
+        }
+    }
+}
