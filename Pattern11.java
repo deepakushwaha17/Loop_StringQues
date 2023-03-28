d0b7410b271d36a7fbce0caf7feef8f015ab0c9c
Deepa <114289926+deepakushwaha17@users.noreply.github.com>
2023-03-28 20:39:18 +0530

    11th commit

--- Pattern11.java
+++ Pattern11.java
@@ -8,7 +8,7 @@ public class Pattern11
 {            
     public static void main(String[] args) 
     {
-        int i,j,k=1;
+        int i,j,k =1;
         for(i=1;i<=5;i++) 
         {
             for(j=1;j<i+1;j++) 
